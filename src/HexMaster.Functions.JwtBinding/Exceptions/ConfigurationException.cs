23578c822d357339fa2c4024b4f29755961c524b
Eduard Keilholz <e.keilholz@4dotnet.nl>
2020-09-21 15:53:52 +0200

    Add more unit tests

---
+++ src/HexMaster.Functions.JwtBinding/Exceptions/ConfigurationException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HexMaster.Functions.JwtBinding.Exceptions
+{
+    public sealed class ConfigurationException : Exception
+    {
+        public ConfigurationException(string message, Exception ex = null) : base(message, ex)
+        {
+        }
+    }
+}
